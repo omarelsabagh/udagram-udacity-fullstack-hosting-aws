ff9a58ba7b6eb90415ce8c648c7f860c5d46b634
[200~omarelsabagh <omarhamid4work@yahoo.com>
2022-09-03 05:57:01 +0200

    circletoeb

---
+++ udagram/udagram-api/bin/ciecle-to-eb.sh
@@ -0,0 +1,11 @@
+eb setenv POSTGRES_HOST=${POSTGRES_HOST}
+# eb setenv POSTGRES_DB=${POSTGRES_DB}
+# eb setenv POSTGRES_USER=${POSTGRES_USER}
+# eb setenv POSTGRES_PASSWORD=${POSTGRES_PASSWORD}
+# eb setenv PORT=${PORT}
+# eb setenv DB_PORT=${DB_PORT}
+# eb setenv JWT_SECRET=${JWT_SECRET}
+# eb setenv AWS_REGION=${AWS_REGION}
+# eb setenv AWS_BUCKET=${AWS_BUCKET}
+# eb setenv AWS_PROFILE=${AWS_PROFILE}
+# eb setenv URL=${URL}
